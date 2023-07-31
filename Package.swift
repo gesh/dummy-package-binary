e6edafa7426100851b3e952c2dcc211674c2c19f
Georgis Andonis <georgis.andonis@gmail.com>
2023-07-31 18:46:34 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,20 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "TestPackage",
+    name: "DummyPackage",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         .library(
-            name: "TestPackage",
-            targets: ["TestPackage"]),
+            name: "DummyPackage",
+            targets: ["DummyPackage"]),
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(
-            name: "TestPackage",
+            name: "DummyPackage",
             url: "https://storage.googleapis.com/ios-swift-sdk/DummyPackage-7.zip",
             checksum: "545ec6634819e715ec443fffb3ce1669311c16ea5c112c331fa625210a6a55e3"
         ),
