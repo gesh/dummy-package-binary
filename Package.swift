66f726fcbd4740c4fdd155bc60cb7354c355aec8
Georgis Andonis <georgis.andonis@gmail.com>
2023-07-31 17:02:46 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "MyPackage",
+            name: "TestPackage",
             url: "https://storage.googleapis.com/ios-swift-sdk/TestPackage-1.zip",
             checksum: "74720fbc6cf58da4ec514aa6df5ab0393c8ebbc9b71c8eb610422a2888524560"
         ),
