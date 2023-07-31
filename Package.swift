2d85cfc0d3bffc6cccf67dc0b26e7247c2ab6eae
Georgis Andonis <georgis.andonis@gmail.com>
2023-07-31 17:02:13 +0300

    Init package binary

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "TestPackage",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "TestPackage",
+            targets: ["TestPackage"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MyPackage",
+            url: "https://storage.googleapis.com/ios-swift-sdk/TestPackage-1.zip",
+            checksum: "74720fbc6cf58da4ec514aa6df5ab0393c8ebbc9b71c8eb610422a2888524560"
+        ),
+    ]
+)
